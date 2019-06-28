1ed03ffe8a42f4a47cfc538041e7e45cc66bde77
danhurtado <51006294+danhurtado@users.noreply.github.com>
2019-06-28 10:08:50 -0500

    Update NewInventoryPage.java

--- src/test/java/pom/Pages/NewInventoryPage.java
+++ src/test/java/pom/Pages/NewInventoryPage.java
@@ -26,7 +26,7 @@ public class NewInventoryPage extends Base {
         type("ID"+randomInteger(),IDNumber);
         submit(EnterNewItemBtn);
         wait(1);
-        assertTrue(isDisplayed(ConfirmRegistration4));
+        assertTrue(isDisplayed(ConfirmRegistration)); //coment
 
     }
 }
